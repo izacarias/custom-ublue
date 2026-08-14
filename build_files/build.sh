fb4b863c324437ff6a03b84b6d118a277dc613ef
izacarias <i.zacarias@tu-bs.de>
2026-08-14 15:47:02 +0200

    fix: error on clen up of usr/systemd/resolve

--- build_files/build.sh
+++ build_files/build.sh
@@ -238,6 +238,5 @@ rm -rf /.gitkeep
 find /var/* -maxdepth 0 -type d \! -name cache \! -name log -exec rm -rf {} \;
 find /var/cache/* -maxdepth 0 -type d \! -name libdnf5 \! -name rpm-ostree -exec rm -rf {} \;
 # Stops resolved to about cleanup cleanup errors
-sudo systemctl stop systemd-resolved
-find /run/* -maxdepth 1 \! -iname resolve -exec rm -rf {} \;
+find /run/* -maxdepth 1 \! -iname resolve -exec rm -rf {} \; 2>/dev/null || true
 # rm -rf /tmp && mkdir -p /tmp
